872ae4a3dca4fc064358abb0699ffa2a43c19f63
Rachid Boudjelida <rachidboudjelida@gmail.com>
2023-07-09 00:09:18 +0100

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import '../app.css';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="min-h-screen p-0 md:p-2">
